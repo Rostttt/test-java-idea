a4342aea8ca5d20994678d167c790f24123df991
Rostttt <rostttttt@gmail.com>
2022-02-12 00:29:19 +0200

    коммит и пуш через графический интерфейс Идеа в 00:29

--- src/MyClass2.java
+++ src/MyClass2.java
@@ -1,6 +1,8 @@
 public class MyClass2 {
     public static void main (String [] args){
         String a = "Пробую настроить Git в Idea";
-        System.out.print(a);
+        System.out.println(a);
+        String b = "внес изменеия в файл в 00:26 и хочу создать вторую ветку";
+        System.out.print(b);
     }
 }
